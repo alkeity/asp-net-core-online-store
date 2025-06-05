5d5a093638b54180a1919a4bdfbe9a6565dfcdda
alkeity <alkeity.23@gmail.com>
2025-06-05 22:13:51 +0300

    feat: json "database" service

---
+++ Services/IDatabaseService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace OnlineStore.Services
+{
+    public interface IDatabaseService
+    {
+        public IEnumerable GetItems(int? amount);
+        public void AddItems(IEnumerable items);
+    }
+}
---
+++ Services/Implementations/JsonDBService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Text.Json;
+using OnlineStore.Models.Domain;
+
+namespace OnlineStore.Services.Implementations
+{
+    public class JsonDBService : IDatabaseService
+    {
+        private string _filePath = "reviews.json";
+
+        public void AddItems(IEnumerable items)
+        {
+            string itemsStr = JsonSerializer.Serialize(items, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_filePath, itemsStr);
+        }
+
+        public IEnumerable GetItems(int? amount)
+        {
+            if (File.Exists(_filePath))
+            {
+                string itemsStr = File.ReadAllText(_filePath);
+                List<Review> items = JsonSerializer.Deserialize<List<Review>>(itemsStr);
+                if (items != null)
+                {
+                    return amount == null ? items : items.GetRange(0, (int)amount);
+                }
+            }
+            return new List<Review>();
+        }
+    }
+}
